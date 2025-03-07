87001c3ddc25a11419da3fe9fa8af1366428ba26
Bankz500 <118247568+Bankz500@users.noreply.github.com>
2025-03-07 07:13:50 +0100

    transfer fix

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,6 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import fs from 'fs';
 
 export default defineConfig({
   build: {
@@ -11,13 +13,13 @@ export default defineConfig({
         deposit: './deposit.html',
         dashboard: './dashboard.html',
         password: './forgot-password.html',
+        transaction: './transaction-success.html',
         profile: './profile.html',
         withdraw: './withdraw.html',
         support: './support.html',
         personal: './personal.html',
         business: './business.html',
         transfer: './transfer.html',
-        manifest: './manifest.json',
         statement: './statement.html',
         profile2: './profile-2.html',
         login: './login.html',
@@ -27,4 +29,16 @@ export default defineConfig({
       },
     },
   },
+  // Copy service-worker.js and manifest.json to dist/
+  plugins: [
+    {
+      name: 'copy-files',
+      writeBundle() {
+        // // Copy service-worker.js
+        // fs.copyFileSync(resolve(__dirname, 'service-worker.js'), resolve(__dirname, 'dist', 'service-worker.js'));
+        // Copy manifest.json
+        fs.copyFileSync(resolve(__dirname, 'manifest.json'), resolve(__dirname, 'dist', 'manifest.json'));
+      },
+    },
+  ],
 });
